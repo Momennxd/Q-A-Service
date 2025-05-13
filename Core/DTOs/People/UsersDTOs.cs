ff1890965f257686cda409a05a81b163bd3c597f
Ahmed Mohamed <ahmedbkry204@gmail.com>
2025-05-13 18:51:23 +0300

    Fix - SendUsersDTO and remove password from it

--- Core/DTOs/People/UsersDTOs.cs
+++ Core/DTOs/People/UsersDTOs.cs
@@ -16,8 +16,6 @@
             public int UserId { get; set; }
 
             public string Username { get; set; }
-            public string Password { get; set; }
-
             public PeopleDTOs.SendPersonDTO Person { get; set; }
 
         }
